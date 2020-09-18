dcceb6b40959cec796d7b31adce322d973914757
Niik <nicraft@tlen.pl>
2020-09-18 17:29:36 +0200

    complete rework

--- nlib/utility/err11.h
+++
@@ -1,6 +0,0 @@
-#ifndef ERR11_H_INCLUDED
-#define ERR11_H_INCLUDED
-
-#include <bits/c++0x_warning.h>
-
-#endif // ERR11_H_INCLUDED
---
+++ threading/include/nlib/threading/buffers.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <atomic>
+
+namespace Nlib
+{
+	namespace Threading
+	{
+		template<typename T> class TrippleBuffer
+		{
+		public:
+			T& nextWriteBuffer() noexcept
+			{
+				write_p = exchange_p.exchange(write_p);
+				return *write_p;
+			}
+			T& currentWriteBuffer() noexcept
+			{
+				return *write_p;
+			}
+
+			T& nextReadBuffer() noexcept
+			{
+				read_p = exchange_p.exchange(read_p);
+				return *read_p;
+			}
+			T& currentReadBuffer() noexcept
+			{
+				return *read_p;
+			}
+
+			bool isLockless() const noexcept
+			{
+				return exchange_p.is_lock_free();
+			}
+
+			TrippleBuffer() : write_p(&first), read_p(&third), exchange_p(&second) {}
+		private:
+			T first, second, third;
+			T* write_p;
+			T* read_p;
+			std::atomic<T*> exchange_p;
+		};
+	}
+}
